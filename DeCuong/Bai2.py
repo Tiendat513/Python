3ebb8abfde2150a031f962f7b0988fa61f452539
Tiendat513 <90968140+Tiendat513@users.noreply.github.com>
2025-03-04 09:45:25 +0000

    Added Bai 2

--- DeCuong/Bai2.py
+++ DeCuong/Bai2.py
@@ -14,8 +14,9 @@ pos2=[]
 for i in range(len(A)):
     temp=sum(A[i])
     if minlor>temp:
-        minlor,pos1=temp, A[i
-    if maxlor<temp:
+        minlor,pos1=temp,A[i]
+        print(pos1)
+    elif maxlor<temp:
         maxlor,pos2=temp,A[i]
 print(f"day co tong max la:{pos2}")
 print(f"day co tong min la:{pos1}")
