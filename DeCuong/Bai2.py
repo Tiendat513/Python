178328992fa0b00b91f5b11f1e06bcb77a31399b
Tiendat513 <90968140+Tiendat513@users.noreply.github.com>
2025-03-04 16:23:39 +0700

    Create Bai2.py

---
+++ DeCuong/Bai2.py
@@ -0,0 +1,22 @@
+n=int(input("n"))
+m=int(input("m"))
+A=[]
+for i in range(n):
+    for j in range(m):
+        temp=[]
+        temp.append(int(input(f"[{i}][{j}]\n")))
+    A.append(temp)
+
+maxlor=0
+minlor=0
+pos1=[]
+pos2=[]
+for i in range(len(A)):
+    temp=sum(A[i])
+    if minlor>temp:
+        minlor,pos1=temp, A[i
+    if maxlor<temp:
+        maxlor,pos2=temp,A[i]
+print(f"day co tong max la:{pos2}")
+print(f"day co tong min la:{pos1}")
+
